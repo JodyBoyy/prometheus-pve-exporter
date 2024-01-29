b39fc9f7e3be36ebeb7c6f0dcc54254d023250c1
Abolfazl Ayazi <tnc.abolfazl@gmail.com>
2024-01-29 16:17:38 +0330

    use multiprocessing

--- src/pve_exporter/__main__.py
+++ src/pve_exporter/__main__.py
@@ -1,10 +1,6 @@
 """
 Proxmox VE exporter for the Prometheus monitoring system.
 """
-import pve_exporter.proxy
-import threading
-from pve_exporter.cli import main
 
-# main()
-t1 = threading.Thread(main())
-t2 = threading.Thread(pve_exporter.proxy.main())
+from pve_exporter.cli import main
+main()
